fbed59ca193a2552b071a3873d2519b1a0f253e3
Ravi Chandra <ravichandra522@gmail.com>
2017-07-15 15:55:00 -0500

    first commit

---
+++ src/main/java/com/javasampleapproach/angularjs/SpringBootAngularJsApplication.java
@@ -0,0 +1,13 @@
+package com.javasampleapproach.angularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+@SpringBootConfiguration
+public class SpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularJsApplication.class, args);
+	}
+}
